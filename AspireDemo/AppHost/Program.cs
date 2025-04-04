18176d31a4b1ffff324680775e7c391b82560b72
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2025-04-04 09:11:04 +0300

    Add the message Q between the APIs.

--- AspireDemo/AppHost/Program.cs
+++ AspireDemo/AppHost/Program.cs
@@ -1,9 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var workerApi = builder.AddProject<Projects.Worker_API>("workerApi");
+var messageQ = builder.AddRabbitMQ("mq");
+
+var workerApi = builder.AddProject<Projects.Worker_API>("workerApi")
+    .WithReference(messageQ);
 
 var workerManagerApi = builder.AddProject<Projects.WorkManagement_API>("workerManagerApi")
-    .WithReference(workerApi);
+    .WithReference(workerApi)
+    .WithReference(messageQ);
 
 var submissionPortal = builder.AddProject<Projects.WorkSubmitPage>("submissionPortal")
     .WithReference(workerManagerApi)
