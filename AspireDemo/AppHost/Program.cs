3ccd843eb8623f853f86675594a1abcc78020216
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2025-04-04 07:50:11 +0300

    Add orchestration.

--- AspireDemo/AppHost/Program.cs
+++ AspireDemo/AppHost/Program.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var workerApi = builder.AddProject<Projects.Worker_API>("workerApi");
+
+var workManagerApi = builder.AddProject<Projects.WorkManagement_API>("workerManagerApi");
+
+var submissionPortal = builder.AddProject<Projects.WorkSubmitPage>("submissionPortal");
+
 builder.Build().Run();
